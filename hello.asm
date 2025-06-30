e9b5fea88050885ae886f85c1a91bdd637e71a9a
melo <elvisbejko@gmail.com>
2025-06-30 19:21:34 +0300

    refactor(hello): add comments

--- hello.asm
+++ hello.asm
@@ -1,22 +1,21 @@
 ; https://board.flatassembler.net/topic.php?t=8506
 
-format ELF executable 3
+format ELF executable 3     ; 32bit ELF executable
 
 segment readable executable
 
-entry start    ; in the original file this was located before the segment. 
-               ; You should put it here instead
+entry start               ; You should put it here instead
 
 start:
-  mov    eax,4
-  mov    ebx,1
-  mov    ecx,msg
-  mov    edx,msg_size
-  int    0x80
+  mov    eax,4            ; syscall number for sys_write
+  mov    ebx,1            ; fd 1 (stdout)
+  mov    ecx,msg          ; pointer to msg
+  mov    edx,msg_size     ; msg length
+  int    0x80             ; interrupt to invoke syscall TODO
 
-  mov    eax,1
-  xor    ebx,ebx
-  int    0x80
+  mov    eax,1            ; syscall number for sys_exit
+  xor    ebx,ebx          ; exit code 0
+  int    0x80             ; perform syscall
 
 segment readable writeable
 
