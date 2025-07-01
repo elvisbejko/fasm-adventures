09a7adb264d983fcfae7109f5630167d98307d9c
Elvis Bejko <ELVISB@amdocs.com>
2025-07-01 04:48:27 +0300

    refactor(hello): fix fmt

--- hello.asm
+++ hello.asm
@@ -4,18 +4,18 @@ format ELF executable 3     ; 32bit ELF executable
 
 segment readable executable
 
-entry start               ; You should put it here instead
+entry start                 ; You should put it here instead
 
 start:
-  mov    eax,4            ; syscall number for sys_write
-  mov    ebx,1            ; fd 1 (stdout)
-  mov    ecx,msg          ; pointer to msg
-  mov    edx,msg_size     ; msg length
-  int    0x80             ; interrupt to invoke syscall TODO
+  mov    eax,4              ; syscall number for sys_write
+  mov    ebx,1              ; fd 1 (stdout)
+  mov    ecx,msg            ; pointer to msg
+  mov    edx,msg_size       ; msg length
+  int    0x80               ; interrupt to invoke syscall TODO
 
-  mov    eax,1            ; syscall number for sys_exit
-  xor    ebx,ebx          ; exit code 0
-  int    0x80             ; perform syscall
+  mov    eax,1              ; syscall number for sys_exit
+  xor    ebx,ebx            ; exit code 0
+  int    0x80               ; perform syscall
 
 segment readable writeable
 
