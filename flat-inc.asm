f7c7ceac0e89e487013222b155eb038813e96637
melo <elvisbejko@gmail.com>
2025-07-02 21:14:59 +0300

    feat(flat): fix fmt

--- flat-inc.asm
+++ flat-inc.asm
@@ -1,8 +1,8 @@
 ; Example of flat binary. 
 ; Compiles to flat-inc.bin not an executable
 
-use32     ; 32bit code setting, 
-          ; if omitted opcode is different
+use32      ; 32bit code setting, 
+           ; if omitted opcode is different
 
 inc     eax
 inc     eax
