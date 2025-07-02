bedd6a05f6224ca57cffb67928fb2ddab11bf9f0
Elvis Bejko <ELVISB@amdocs.com>
2025-07-02 17:29:54 +0300

    feat(flat-bin): add comments to flat-bin

--- flat-inc.asm
+++ flat-inc.asm
@@ -1,6 +1,9 @@
 ; Example of flat binary. 
 ; Compiles to flat-inc.bin not an executable
 
+use32     ; 32bit code setting, 
+          ; if omitted opcode is different
+
 inc    eax
 inc    eax
 inc    eax
