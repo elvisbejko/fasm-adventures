ce4f094a3f33c6e4490d4f0225d9f0f70448e13d
melo <elvisbejko@gmail.com>
2025-07-02 21:14:20 +0300

    feat(flat): add xchg eax,eax aka nop

--- flat-inc.asm
+++ flat-inc.asm
@@ -4,9 +4,12 @@
 use32     ; 32bit code setting, 
           ; if omitted opcode is different
 
-inc    eax
-inc    eax
-inc    eax
-inc    eax
-inc    eax
-dec    eax
+inc     eax
+inc     eax
+inc     eax
+inc     eax
+inc     eax
+dec     eax
+xchg    eax,eax    ; rax won't work in 32bit mode
+xchg    eax,eax    ; rax won't work in 32bit mode
+nop
