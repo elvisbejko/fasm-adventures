9aade62ae1e70cd1841b0f26170e9997f045407a
Elvis Bejko <ELVISB@amdocs.com>
2025-07-02 17:26:54 +0300

    feat(flat-bin): add flat bin v2

--- flat-inc.asm
+++ flat-inc.asm
@@ -1,6 +1,8 @@
-; Example of flat binary
+; Example of flat binary. 
+; Compiles to flat-inc.bin not an executable
   inc    eax
   inc    eax
   inc    eax
   inc    eax
   inc    eax
+  dec    eax
